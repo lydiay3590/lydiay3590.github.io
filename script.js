2f4175c78f2ea6d5f44f2b6c996947fc8d038219
lydiay3590 <43858696+lydiay3590@users.noreply.github.com>
2019-10-07 10:24:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Lydia Ye"
+let userName = "lydiay3590";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
